9535c1d89f58ab7cdc3259233efc0f02c446d8a1
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-10-10 21:06:10 +0100

    create next app

---
+++ app_number_one/app-one/src/app/newroute/page.tsx
@@ -0,0 +1,12 @@
+const NewRoute = () => {
+  console.warn("Executing new route...");
+  return (
+    <div>
+      <main>
+        <h1>New Route</h1>
+      </main>
+    </div>
+  );
+};
+
+export default NewRoute;
