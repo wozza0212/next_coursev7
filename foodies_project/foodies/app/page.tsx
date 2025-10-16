6cdac05fd5cbcf400eb32a3424e5f2d8166af6bc
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-10-16 14:52:21 +0100

    slideshow and community page

--- foodies_project/foodies/app/page.tsx
+++ foodies_project/foodies/app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import ImageSlideshow from "../components/Slideshow/image-slides";
 const Home = () => {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.slideshow}></div>
+        <div className={styles.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={styles.hero}>
             <h1>Next Level Food for Foodies</h1>
