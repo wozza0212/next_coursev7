cef579feb3e1a11da077e4f7b6707f572eaec695
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-10-17 20:50:30 +0100

    add meals grid

--- foodies_project/foodies/app/meals/page.tsx
+++ foodies_project/foodies/app/meals/page.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "../../components/Meals/meals-grid";
+
 const MealsPage = () => {
   return (
-    <div>
-      <h2>Meals Page</h2>
-    </div>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicous meals, created by{" "}
+          <span className={styles.highlight}>By you</span>
+        </h1>
+        <p>Choose you favourite Recipe and cook it yourself</p>
+        <p className={styles.cta}>
+          <Link href="meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
-export default MealsPage
+export default MealsPage;
---
+++ foodies_project/foodies/components/Meals/meal-types.ts
@@ -0,0 +1,8 @@
+export interface MealItemType {
+    title: string,
+    slug: string,
+    image: string,
+    summary: string,
+    creator: string,
+    id: string
+}
