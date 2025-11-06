88d6ce4864083e901d7f62cc8f5bc34b2142c896
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-11-06 21:43:34 +0000

    creat and handle custome error

--- foodies_project/foodies/lib/meals.ts
+++ foodies_project/foodies/lib/meals.ts
@@ -1,10 +1,20 @@
 import sql from "better-sqlite3";
 
+class DatabaseError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "Database Loading Error";
+    this.message = "Error Loading Database";
+    Object.setPrototypeOf(this, DatabaseError.prototype);
+  }
+}
+
 const db = sql("meals.db");
 
 const getMeals = async () => {
   // Add timeout to mimic real world loading times, and to use async
   await new Promise((resolve) => setTimeout(resolve, 2000));
+  // throw new DatabaseError("database laoding error");
   return db.prepare("SELECT * FROM meals").all();
 };
 
